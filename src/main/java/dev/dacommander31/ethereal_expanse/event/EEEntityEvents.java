d138e5949c22ed2aa1dd5d4bc9c7c0db37452851
Apmaster2 <nasrinmh83@gmail.com>
2025-11-29 17:26:03 +1100

    Mixin

--- src/main/java/dev/dacommander31/ethereal_expanse/event/EEEntityEvents.java
+++ src/main/java/dev/dacommander31/ethereal_expanse/event/EEEntityEvents.java
@@ -3,23 +3,12 @@ package dev.dacommander31.ethereal_expanse.event;
 import dev.dacommander31.ethereal_expanse.EtherealExpanse;
 import dev.dacommander31.ethereal_expanse.item.EEItems;
 import dev.dacommander31.ethereal_expanse.network.EEAttachmentTypes;
-import net.minecraft.ChatFormatting;
-import net.minecraft.advancements.critereon.PlayerHurtEntityTrigger;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.event.entity.EntityEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
-import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
-
-import java.awt.*;
-import java.util.Objects;
 
 @EventBusSubscriber(modid = EtherealExpanse.MOD_ID)
 public class EEEntityEvents {
@@ -32,7 +21,7 @@ public class EEEntityEvents {
 
         if (weaponItem != null && weaponItem.is(EEItems.LEVIATHAN_BOW)) {
             //TODO: Add bomb planting
-            entity.setData(EEAttachmentTypes.LEVIATAN_BOMB_ATTACHED.get(), true);
+            entity.setData(EEAttachmentTypes.LEVIATHAN_BOMB_TIME, 60);
             EtherealExpanse.LOGGER.info("damaged by leviathan bow");
         }
     }
--- src/main/java/dev/dacommander31/ethereal_expanse/network/EEAttachmentTypes.java
+++ src/main/java/dev/dacommander31/ethereal_expanse/network/EEAttachmentTypes.java
@@ -1,9 +1,7 @@
 package dev.dacommander31.ethereal_expanse.network;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
 import dev.dacommander31.ethereal_expanse.EtherealExpanse;
-import net.minecraft.world.entity.animal.Cod;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.attachment.AttachmentType;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -12,10 +10,16 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import java.util.function.Supplier;
 
 public class EEAttachmentTypes {
+
     public static final DeferredRegister<AttachmentType<?>> ATTACHMENT_TYPES = DeferredRegister.create(NeoForgeRegistries.ATTACHMENT_TYPES, EtherealExpanse.MOD_ID);
 
-    public static final Supplier<AttachmentType<Boolean>> LEVIATAN_BOMB_ATTACHED = ATTACHMENT_TYPES.register("leviathan_bomb_attached",
-            () -> AttachmentType.builder(() -> false).serialize(Codec.BOOL.fieldOf("leviathan_bomb_attached")).build());
+    public static final Supplier<AttachmentType<Integer>> LEVIATHAN_BOMB_TIME = ATTACHMENT_TYPES.register(
+            "leviathan_bomb_time",
+            () -> AttachmentType.builder(
+                    () -> {EtherealExpanse.LOGGER.info("NEW DATA"); return 0;})
+                    .serialize(Codec.INT.fieldOf("leviathan_bomb_time"))
+                    .build()
+    );
 
     public static void register(IEventBus eventBus) {
         ATTACHMENT_TYPES.register(eventBus);
